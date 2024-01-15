0707df37b0f8c48fad797d7979ba207456682f5a
Jerald Lee <jleeshancheng@gmail.com>
2024-01-16 00:29:30 +0800

    fix(app/page.tsx): Change Link Href

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,26 @@
-  "use client"
+"use client";
 
-  import Link from "next/link";
-  import { ViewAnnouncements } from "./_components/view-announcements";
-  import { LoginPage } from "./_components/login";
+import Link from "next/link";
+import { ViewAnnouncements } from "./_components/view-announcements";
+import { LoginPage } from "./_components/login";
 
-  export default function Home() {
-
-    return (
-      <div className="relative">
-        <div className="flex justify-end mt-4 mr-4 absolute top-0 right-0">
-          {/* <Link href="/create" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block">      
+export default function Home() {
+  return (
+    <div className="relative">
+      <div className="absolute right-0 top-0 mr-4 mt-4 flex justify-end">
+        {/* <Link href="/create" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-block">      
               Create Announcement              
           </Link> */}
-          <Link href="/admin" className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded inline-block">
-            Admin
-          </Link>
-        </div>
-        {/* <LoginPage /> */}
-      
-        <ViewAnnouncements />
+        <Link
+          href="/admin/view"
+          className="inline-block rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+        >
+          Admin
+        </Link>
       </div>
-    );
-  }
+      {/* <LoginPage /> */}
+
+      <ViewAnnouncements />
+    </div>
+  );
+}
