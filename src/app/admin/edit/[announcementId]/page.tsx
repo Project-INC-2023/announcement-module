0ff3d2e7ddb25bb0767ef940ce87a49769202818
Jerald Lee <jleeshancheng@gmail.com>
2024-02-06 01:03:22 +0800

    chore (adminEditAnnouncement): Clean up code not used

--- src/app/admin/edit/[announcementId]/page.tsx
+++ src/app/admin/edit/[announcementId]/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 import { api } from "@/trpc/react";
 import EditAnnouncement from "@/app/_components/edit-announcement";
-import { getServerAuthSession } from "@/server/auth";
 
 const AdminEdit: React.FC = () => {
   const router = useRouter();
