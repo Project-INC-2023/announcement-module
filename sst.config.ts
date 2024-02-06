37fb9ae7cdc05e5a85ccfef21ca93fe9b55c9dc1
Jerald Lee <jleeshancheng@gmail.com>
2024-02-06 10:06:16 +0800

    fix(sst.config.ts): Spelling error

--- sst.config.ts
+++ sst.config.ts
@@ -13,7 +13,7 @@ export default {
       const site = new NextjsSite(stack, "site", {
         environment: {
           DATABASE_URL: process.env.DATABASE_URL as string,
-          NEXTAUTH_URL: process.env.NEXTAUTH_UR as string,
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL as string,
           NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET as string,
           GITHUB_ID: process.env.GITHUB_ID as string,
           GITHUB_SECRET: process.env.GITHUB_SECRET as string,
