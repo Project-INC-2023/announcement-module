d384cfdfddc4b4be45cc348765c752c5b1bc7a39
Jerald Lee <jleeshancheng@gmail.com>
2024-02-06 22:20:40 +0800

    style(UserViewAnnouncements): Adding Shadcn components

--- src/app/viewAnnouncement/UserViewAnnouncement.tsx
+++ src/app/viewAnnouncement/UserViewAnnouncement.tsx
@@ -1,13 +1,22 @@
 import { redirect } from "next/navigation";
 import ViewAnnouncements from "../_components/view-announcements";
 import { getServerAuthSession } from "@/server/auth";
+import ProfileDropDownMenu from "@/app/_components/profileDropDownMenu";
 
 const UserPage = async () => {
   const session = (await getServerAuthSession())!;
   if (!session?.user) return redirect("/");
 
   return (
-    <div className="relative flex">
+    <div className="relative flex flex-col">
+      <div className="flex bg-gray-200 px-10 py-2">
+        <div className="flex w-1/4 justify-start ">
+          <ProfileDropDownMenu image={session.user.image} />
+        </div>
+        <h2 className="flex w-1/2 items-center justify-center text-xl font-semibold">
+          View Announcements
+        </h2>
+      </div>
       <div className="w-full items-center justify-between pt-3">
         <ViewAnnouncements />
       </div>
