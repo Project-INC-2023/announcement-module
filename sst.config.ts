0e3170ac145749817f287914bdf09424641ba281
Jerald Lee <jleeshancheng@gmail.com>
2024-02-06 10:11:04 +0800

    fix(sst.config.ts): Naming for Env Var

--- sst.config.ts
+++ sst.config.ts
@@ -15,8 +15,8 @@ export default {
           DATABASE_URL: process.env.DATABASE_URL as string,
           NEXTAUTH_URL: process.env.NEXTAUTH_URL as string,
           NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET as string,
-          GITHUB_ID: process.env.GITHUB_ID as string,
-          GITHUB_SECRET: process.env.GITHUB_SECRET as string,
+          GITHUB_CLIENT_ID: process.env.GITHUB_ID as string,
+          GITHUB_CLIENT_SECRET: process.env.GITHUB_SECRET as string,
         },
       });
 
