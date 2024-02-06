a23fdbcde205f7170c8a98b727166b3798076af9
Jerald Lee <jleeshancheng@gmail.com>
2024-02-06 15:28:59 +0800

    style(AdminViewAnnouncement): Implementing Shadcn to page

--- src/app/viewAnnouncement/AdminViewAnnouncement.tsx
+++ src/app/viewAnnouncement/AdminViewAnnouncement.tsx
@@ -5,6 +5,16 @@ import { redirect } from "next/navigation";
 import AdminViewAnnouncements from "@/app/_components/admin-view-announcements";
 import { getServerAuthSession } from "@/server/auth";
 import SignOutButton from "@/app/_components/signOutButton";
+import { Avatar, AvatarFallback, AvatarImage } from "@/_components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/_components/ui/dropdown-menu";
+import { Button } from "@/_components/ui/button";
 
 const AdminPage: React.FC = async () => {
   const session = (await getServerAuthSession())!;
@@ -12,17 +22,33 @@ const AdminPage: React.FC = async () => {
 
   return (
     <div className="max-w mx-auto max-h-screen text-center">
-      <div className="flex items-center justify-between bg-gray-200 py-4">
-        <h2 className="ml-44 flex-grow text-xl font-semibold">Admin page</h2>
+      <div className="flex items-center justify-between bg-gray-200 px-10 py-2">
+        <div className="flex w-1/4 justify-start">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="outline-none">
+              <Avatar className="h-12 w-12">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Sign Out</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <h2 className="w-1/2 flex-grow text-xl font-semibold">Admin page</h2>
 
-        <div className="flex items-center">
-          <SignOutButton />
-          <Link
-            href="admin/create"
-            className="mx-2 rounded border-2 border-green-500 px-4 py-2 font-bold text-green-500 hover:bg-green-700"
-          >
-            Create
-          </Link>
+        <div className="flex w-1/4 justify-end">
+          <Button className="p-0">
+            <Link href="admin/create" className="px-3">
+              Create
+            </Link>
+          </Button>
         </div>
       </div>
 
